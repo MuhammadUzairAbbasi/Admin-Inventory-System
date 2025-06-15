796186047e19c949b735975e515ad43ea17b7c29
Uzairabbasi25 <k214548@nu.edu.pk>
2025-06-15 12:36:52 +0500

    api Url secured

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_BASE_URL ||
-    "https://admin-inventory-system-production.up.railway.app/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api",
   headers: {
     "Content-Type": "application/json",
   },
